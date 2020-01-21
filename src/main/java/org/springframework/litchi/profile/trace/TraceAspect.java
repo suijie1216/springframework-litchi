e39eba4837ed8c1c12338eb4875b59223bf98a3a
suijie.sj <suijie.sj@cainiao.com>
2020-01-21 12:16:13 +0800

    opti

--- src/main/java/org/springframework/litchi/profile/trace/TraceAspect.java
+++ src/main/java/org/springframework/litchi/profile/trace/TraceAspect.java
@@ -1,7 +1,6 @@
 package org.springframework.litchi.profile.trace;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
